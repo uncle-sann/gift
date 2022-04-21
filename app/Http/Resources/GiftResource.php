7abc147fb1877bdc5c439f1b8be1d9a21ddc6727
Babanyyaz <camanow259@gmail.com>
2022-04-22 02:20:22 +0500

    return gifts if children exists

--- app/Http/Resources/GiftResource.php
+++ app/Http/Resources/GiftResource.php
@@ -6,18 +6,23 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class GiftResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
-    public function toArray($request)
-    {
-        return [
-           'id' => $this->id,
-           'name' => $this->name,
-           'gifts' => GiftResource::collection($this->children), 
-        ];
+  /**
+   * Transform the resource into an array.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+   */
+  public function toArray($request)
+  {
+    $result =  [
+      'id' => $this->id,
+      'name' => $this->name,
+    ];
+
+    if (count($this->children)) {
+      $result['gifts'] = GiftResource::collection($this->children);
     }
+
+    return $result;
+  }
 }
